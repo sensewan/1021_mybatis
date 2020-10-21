427f1ae4db84b8d357bf9b2aff52f7567052cf59
sensewan <sensewan@gmail.com>
2020-10-21 14:35:34 +0900

    map change json

---
+++ src/main/java/com/test/db/IndexController.java
@@ -0,0 +1,49 @@
+package com.test.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import mybatis.dao.MemDAO;
+import mybatis.vo.MemVO;
+
+// rootconext에 의해 움직이지 않음 servlet-context에 의해 움직임
+
+@Controller
+public class IndexController {
+	
+	
+	// ↱ DAO사용하는 것으로 자동으로 DAO 만들어진거 들어옴 (servlet-context.xml에 <annotation-driven/>이 있어서 가능함)
+	@Autowired
+	private MemDAO m_dao;
+	
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+	
+	
+                                      // ↱이렇게 하면 total()은 무조건 POST방식으로만 호출해야 한다.
+	@RequestMapping(value = "/total", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> total(){
+		              // ↳머가 들어올지 모르므로..배열도 올수 있고 등 (사실 memvo[]로 해도 됨)
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		// DB사용하기 (DAO사용하기)
+		
+		MemVO[] ar = m_dao.getAll();
+		map.put("list", ar);
+		
+		return map;  // 여기 total은 index.jsp의 $.ajax비동기식 통신으로 호출되어 졌고 반환을 JSON으로 해야하지만
+					 // 현재 Map으로 하고 있다 . 그러므로 Map을 -> JSON으로 변환해야 한다.
+		
+	}
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,82 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>index.jsp</title>
+
+<style>
+	table {
+		width: 350px;
+		border-collapse: collapse;
+	}
+	
+	table th, table td{
+		border: 1px solid black;
+	}
+</style>
+
+</head>
+<body>
+
+	<h1>MyBatis- Spring과 비동기식 통신 연습</h1>
+	
+	<table id ="t1">
+		<colgroup>
+			<col width="15%"/>
+			<col width= "25%"/>
+			<col width="*"/>
+		</colgroup>
+		
+		<thead>
+			<tr>
+				<td colspan="3">
+					<input type="button" value="전체" id="total_btn" />
+				</td>
+			</tr>
+			<tr>
+				<th>번호</th>
+				<th>아이디</th>
+				<th>비번</th>
+			</tr>
+		</thead>
+		
+		<tbody>
+			
+		</tbody>
+		
+	</table>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+	$(function() {
+		
+		//비동기 통신으로 컨트롤러 실행해서 DAO실행시켜 DB접근하기.
+		
+		// total_btn을 클릭했을 때 실행되기... (bind & unbind 사용가능..)
+		$("#total_btn").bind("click", function() {
+			
+			$.ajax({
+				url:"total",
+				type: "post",
+				dataType: "json"				
+			}).done(function(data) {
+				console.log(data);
+			});
+		});
+	});
+</script>
+
+
+	
+</body>
+</html>
+
+
+
+
+
+
+
